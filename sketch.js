1e19b3999b3a7d8062c45298775acdc02968691a
divyavala <divyavala@users.noreply.github.com>
2022-04-28 18:08:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,11 +8,5 @@ function draw() {
 
 function count()
 {
-  let numbers = [1,2,3,4,5,6,7,8,9];
-  let len = numbers.length;
-
-  for(let i = 0; i<len; i++)
-  {
-    console.log(numbers[i]);
-  }
+ 
 }
